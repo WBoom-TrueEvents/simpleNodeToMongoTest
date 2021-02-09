ec0ee347d366677661f99dd255d5f477df4fbe36
Chris Cullen <chris@skylarkcreative.co.uk>
2021-02-09 18:19:07 +0000

    Fixed status messages

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ let connection;
   console.log("Begin connection to mongodb on atlas using x509 certificate");
   try {
     connection = await mongodb.makeMongoConnection();
+    console.log("Connection to mongo established")
   } catch (err) {
     console.error("There was an error connecting to mongoDB");
     console.error(err.message);
   }
-  console.log(`mongo connection isConnected is ${connection.isConnected}`);
+  console.log(`mongo connection isConnected is ${connection.isConnected()}`);
   await connection.close(true)
+  console.log("Disconnected from mongo")
 })();
